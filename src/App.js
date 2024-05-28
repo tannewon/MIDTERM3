23b42b8aa4d15704a5e02a827d094e0de17e8cce
Tan <tan.a25@student.passerellesnumeriques.org>
2024-05-28 22:07:02 +0700

    update user

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
+import axios from "axios";
+import { useEffect } from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
-function App() {
+// App.js
+const App = () => {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("https://api.github.com/users");
+        setUsers(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <div className="App">
       <Navbar />
       <div className="container">
-        <h1>Hello from React</h1>
+        <h1>GitHub Users Data</h1>
       </div>
+      <Users users={users} />
     </div>
   );
-}
+};
 export default App;
