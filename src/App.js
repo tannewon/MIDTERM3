c5fe759cf7aa48d28e8a3df0a8ff75d24c688207
Tan <tan.a25@student.passerellesnumeriques.org>
2024-05-28 22:10:59 +0700

    update file search

--- src/App.js
+++ src/App.js
@@ -3,26 +3,19 @@ import { useEffect } from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 // App.js
+// App.js
+import React from "react";
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Search from "./components/users/Search";
 const App = () => {
-  const [users, setUsers] = useState([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get("https://api.github.com/users");
-        setUsers(response.data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-    fetchData();
-  }, []);
   return (
     <div className="App">
       <Navbar />
       <div className="container">
         <h1>GitHub Users Data</h1>
+        <Search />
       </div>
-      <Users users={users} />
     </div>
   );
 };
