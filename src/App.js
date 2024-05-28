e8a4b8902e143fa0e94def918273dcc16e65bd96
Tan <tan.a25@student.passerellesnumeriques.org>
2024-05-28 22:22:44 +0700

    update file search second

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import axios from "axios";
-import { useEffect } from "react";
-import "./App.css";
-import Navbar from "./components/layout/Navbar";
-// App.js
-// App.js
 import React from "react";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Search from "./components/users/Search";
 const App = () => {
   return (
     <div className="App">
-      <Navbar />
-      <div className="container">
-        <h1>GitHub Users Data</h1>
-        <Search />
-      </div>
+      <Router>
+        <Navbar />
+        <div className="container">
+          <h1>GitHub Users Data</h1>
+          <Switch>
+            <Route exact path="/" component={Search} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 };
--- src/components/users/Search.jsx
+++ src/components/users/Search.jsx
@@ -6,42 +6,27 @@ const Search = () => {
   const [text, setText] = useState("");
   const [users, setUsers] = useState([]);
   const searchUsers = async (text) => {
-    try {
-      const response = await axios.get(
-        `https://api.github.com/search/users?q=${text} `
-      );
-      setUsers(response.data.items);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
+    // .. Rest of the code
+  };
+  const clearUsers = () => {
+    setUsers([]);
   };
   const onSubmit = (e) => {
-    e.preventDefault();
-    if (text === "") {
-      alert("Please enter something");
-    } else {
-      searchUsers(text);
-      setText("");
-    }
+    // ... rest of the code
   };
   const onChange = (e) => setText(e.target.value);
+
   return (
     <div>
       <form onSubmit={onSubmit} className="form">
-        <input
-          type="text"
-          name="text"
-          placeholder="Search User"
-          value={text}
-          onChange={onChange}
-        />
-
-        <input
-          type="submit"
-          value="Search"
-          className="btn btn-success btn-block"
-        />
+        {/*<!..> ... The rest of the form */}
       </form>
+      {/*Adding Clear button */}
+      {users.length > 0 && (
+        <button className="btn btn-danger btn-block" onClick={clearUsers}>
+          Clear
+        </button>
+      )}
       <Users users={users} />
     </div>
   );
